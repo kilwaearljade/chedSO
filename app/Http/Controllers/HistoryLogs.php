c866c08896c82a94360eacccb0c2b65c27113891
earl <earljadetumanda08@gmail.com>
2026-01-07 22:08:59 +0800

    make controller

---
+++ app/Http/Controllers/HistoryLogs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HistoryLogs extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+}
